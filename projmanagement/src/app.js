5dbf0385c81a428bd4bf0921c30cdc85ac4ed234
KRISHNA <kraggr2909@gmail.com>
2026-01-29 20:37:12 +0530

    configurations are done

--- projmanagement/src/app.js
+++ projmanagement/src/app.js
@@ -1,6 +1,28 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
+// app.use middleware - BASIC CONFIGURATION
+
+//to accept json data 
+app.use(express.json({ limit: "16kb" }))
+
+//to accept data from url + encoding like spaces into %20 and all
+app.use(express.urlencoded({extended: true, limit : "16kb"}))
+
+// to make public folder publicly viewable such that i can serve content from it directly
+app.use(express.static("public"))
+
+// CORS CONFIGURATION
+app.use(cors({
+    origin : process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods : ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"],
+}))
+
+
 
 app.get("/", (req,res) => {
     res.send("Welcome to base camp!")
