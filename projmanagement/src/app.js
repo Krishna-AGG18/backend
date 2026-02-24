003ecc6ef0ddd36b09e10807dada02f02dd583ba
KRISHNA <kraggr2909@gmail.com>
2026-02-24 13:59:43 +0530

    healthcheck

--- projmanagement/src/app.js
+++ projmanagement/src/app.js
@@ -1,35 +1,41 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
 // app.use middleware - BASIC CONFIGURATION
 
-//to accept json data 
-app.use(express.json({ limit: "16kb" }))
+//to accept json data
+app.use(express.json({ limit: "16kb" }));
 
 //to accept data from url + encoding like spaces into %20 and all
-app.use(express.urlencoded({extended: true, limit : "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // to make public folder publicly viewable such that i can serve content from it directly
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // CORS CONFIGURATION
-app.use(cors({
-    origin : process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
-    credentials: true,
-    methods : ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-    allowedHeaders: ["Content-Type","Authorization"],
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
 
+// import the routes
 
+import healthCheckRouter from "./routes/healthcheck.routes.js";
 
-app.get("/", (req,res) => {
-    res.send("Welcome to base camp!")
-})
+app.use("/api/vi/healthcheck", healthCheckRouter);
 
-app.get("/instagram", (req,res) => {
-    res.send("Welcome to base camp's insta page!")
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to base camp!");
+});
+
+app.get("/instagram", (req, res) => {
+    res.send("Welcome to base camp's insta page!");
+});
 
 export default app;
--- projmanagement/src/routes/healthcheck.routes.js
+++ projmanagement/src/routes/healthcheck.routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { healthCheck } from "../controllers/healthcheck.controller.js";
 const router = Router();
 
-router
+router.route("/").get(healthCheck)
 
 export default router;
