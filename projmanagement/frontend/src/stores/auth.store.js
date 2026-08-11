24c8b4285f7042ddb96468867580c40dc420c91d
Krishna-AGG18 <kraggr2909@gmail.com>
2026-08-11 09:46:01 +0530

    set : state

---
+++ projmanagement/frontend/src/stores/auth.store.js
@@ -0,0 +1,31 @@
+import {create} from "zustand";
+import {devtools, persist} from 'zustand/middleware';
+
+export const useAuthStore = create(
+    devtools(
+        persist(
+            (set) => ({
+                //initial state
+                user : null,
+                isAuthenticated : false,
+
+                //action
+                setLogin : (userDate, token) => {
+                    localStorage.setItem('accessToken', token);
+                    set({ user: userData, isAuthenticated: true });
+                },
+
+                setLogout : () =>{
+                    localStorage.removeItem('accessToken');
+                    set({ user: null, isAuthenticated: false });
+                },
+                
+                updateUser : (newData) => 
+                    set((state) => ({user : {...state.user, ...newData}}))
+            }),
+            {
+                name: 'auth-storage',
+            }
+        )
+    )
+)
